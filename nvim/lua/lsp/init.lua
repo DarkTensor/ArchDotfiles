463cda7effce30bb27436df40770c3638d139109
DarkTensor <a@Ants-MacBook-Pro.local>
2023-11-12 04:44:27 -0500

    added lsp keymappings. stable

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -4,10 +4,19 @@ require("mason-lspconfig").setup({
 	ensure_installed = { "clangd", "lua_ls" }
 })
 
+
 -- After setting up mason-lspconfig you may set up servers via lspconfig
 -- require("lspconfig").lua_ls.setup {}
 -- require("lspconfig").rust_analyzer.setup {}
-
-
 require("lspconfig").clangd.setup {}
-require("lspconfig").lua_ls.setup {}
+require("lspconfig").lua_ls.setup {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' }
+			}
+		}
+	}
+}
+
+require("lsp.lsp-keymaps")
---
+++ nvim/lua/lsp/lsp-keymaps.lua
@@ -0,0 +1,73 @@
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+
+	local opts = {}
+	vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)							-- shows diagnostic behind cursor
+	vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)							-- shows diagnostic in front of cursor
+	vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)			-- shows diagnostic local list
+
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'H', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+	  vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)            -- shows line diagnostic
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    --vim.keymap.set('n', '<space>wl', function()
+     -- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    -- end, opts)
+    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    -- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    -- vim.keymap.set('n', '<space>f', function()
+      -- vim.lsp.buf.format { async = true }
+    -- end, opts)
+  end,
+})
+
+local signs = {
+    { name = "DiagnosticSignError", text = "" },
+    { name = "DiagnosticSignWarn", text = "" },
+    { name = "DiagnosticSignHint", text = "" },
+    { name = "DiagnosticSignInfo", text = "" },
+  }
+
+  for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  end
+
+ local config = {
+    -- disable virtual text
+    virtual_text = true,
+    -- show signs
+    signs = signs,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = false,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  }
+
+  vim.diagnostic.config(config)
+
+
